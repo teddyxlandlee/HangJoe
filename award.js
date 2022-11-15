b6601abecace567f3582652cbcd7fa8239908a5b
teddyxlandlee <tedexe_work.top@foxmail.com>
2022-11-15 15:58:48 +0800

    award

--- award.js
+++ award.js
@@ -5,13 +5,22 @@ const queries = _squeries(window.location.href);
 
 var _doc = document.getElementById('main0').innerHTML;
 var words = {};
-document.getElementById('main0').innerHTML = 'Loading...'
+document.getElementById('main0').innerHTML = 'Loading...';
+var maxNum0 = -1;
 
 if (!queries.wl) _err();
 _readFromUrl(queries.wl, function(c) {
     //words = _readWordList(c);
     var ss = Base64.decode(c);
-    words = JSON.parse(ss);
+    var w0 = JSON.parse(ss);
+    words = {};
+    let i = 0;
+    for (x in w0) {
+        i = i + w0[x];
+        words[x] = i;
+    }
+    maxNum0 = i;
+
     document.getElementById('main0').innerHTML = _doc;
 }, _err);
 
@@ -23,10 +32,20 @@ function roll() {
             var l = _randint(10000000, 99999999);
             document.getElementById('mainNumber').innerHTML = String(l);
         } else {
-            var l = _randint(0, words.keys.length)
-            var r = words[words.keys[l]];
-            if (r == null) window.location.href = 'https://shattereddisk.github.io/rickroll/rickroll.mp4';
-            document.getElementById('mainNumber').innerHTML = r;
+            var l = _randint(0, maxNum0 - 1);
+            var cc = '???';
+            var dd = -1;
+            for (x in words) {
+                let ee = words[x]; // int
+                if (ee > dd && ee <= l) {
+                    dd = ee;
+                    cc = x;
+                }
+            }
+            //var l = _randint(0, words.keys.length)
+            //var r = words[words.keys[l]];
+            //if (r == null) window.location.href = 'https://shattereddisk.github.io/rickroll/rickroll.mp4';
+            document.getElementById('mainNumber').innerHTML = String(x);
         }
     });
 }
